7cf373735fe61936c35430b6428614ccf91317ca
Liam Cain <hi@liamca.in>
2020-09-07 23:54:07 -0400

    improve styling, fix empty rows for some months

--- src/Calendar.svelte
+++ src/Calendar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import moment from "moment";
+  import moment, { Moment } from "moment";
   import type { Leaf, Vault } from "./obsidian";
 
   export let activeLeaf: Leaf = null;
@@ -10,46 +10,63 @@
   export let format: string;
 
   const today = moment();
-  const startDate = moment({ day: 1 });
   const activeFile = activeLeaf?.view.file?.path;
+  export let displayedMonth: Moment = today;
 
   let days = [];
-  let offset = startDate.isoWeekday() + 1;
-  let day = 1;
-  for (let weekNum = 0; weekNum <= 5; weekNum++) {
-    const daysInWeek = [];
-    days.push(daysInWeek);
+  let monthName: string;
 
-    for (let weekday = 1; weekday <= 7; weekday++) {
-      // Insert empty objects for spacers
-      if (weekNum * 6 + weekday < offset || day > startDate.daysInMonth()) {
-        daysInWeek.push({});
-        continue;
+  $: {
+    days = [];
+    const startDate = displayedMonth.clone().date(1);
+    let offset = startDate.isoWeekday() + (1 % 7);
+    let day = 1;
+
+    for (let weekNum = 0; weekNum <= 5; weekNum++) {
+      const daysInWeek = [];
+      days.push(daysInWeek);
+
+      for (let weekday = 1; weekday <= 7; weekday++) {
+        // Insert empty objects for spacers
+        if (weekNum * 6 + weekday < offset || day > startDate.daysInMonth()) {
+          daysInWeek.push({});
+          continue;
+        }
+
+        const { path } = vault.adapter;
+
+        const date = displayedMonth.clone().date(day);
+        const formattedDate = `${date.format(format)}.md`;
+        const fileForDay = vault.getAbstractFileByPath(
+          path.join(directory, formattedDate)
+        );
+        const fileSize = fileForDay?.stat?.size || 0;
+
+        daysInWeek.push({
+          dayOfMonth: day,
+          date,
+          formattedDate,
+          numDots: fileSize ? Math.floor(Math.log(fileSize / 20)) : 0,
+        });
+
+        day++;
       }
 
-      const { path } = vault.adapter;
-      const formattedDate = `${moment({ day }).format(format)}.md`;
-      const fileForDay = vault.getAbstractFileByPath(
-        path.join(directory, formattedDate)
-      );
-      const fileSize = fileForDay?.stat?.size || 0;
-
-      daysInWeek.push({
-        dayOfMonth: day,
-        formattedDate: `${moment({ day }).format(format)}.md`,
-        isActiveFile: activeFile === formattedDate,
-        numDots: fileSize ? Math.floor(Math.log(fileSize / 20)) : 0,
-      });
-
-      day++;
+      if (day >= startDate.daysInMonth()) {
+        break;
+      }
     }
 
-    if (day >= startDate.daysInMonth()) {
-      break;
-    }
+    monthName = displayedMonth.format("MMM YYYY");
   }
 
-  const monthName = moment().format("MMM YYYY");
+  function incrementMonth() {
+    displayedMonth = displayedMonth.add(1, "months");
+  }
+
+  function decrementMonth() {
+    displayedMonth = displayedMonth.subtract(1, "months");
+  }
 </script>
 
 <style>
@@ -58,8 +75,22 @@
     text-align: center;
   }
 
+  .title {
+    margin-right: 4px;
+    text-align: center;
+  }
+
   .today {
     background: var(--purple);
+  }
+
+  .arrow {
+    cursor: pointer;
+    display: inline-block;
+  }
+  .arrow svg {
+    height: 16px;
+    width: 16px;
   }
 
   .active {
@@ -114,10 +145,41 @@
   .dot:last-of-type {
     margin-right: 0;
   }
+
+  .ml-2 {
+    margin-left: 8px;
+  }
+
+  .mr-2 {
+    margin-right: 8px;
+  }
 </style>
 
 <div class="container">
-  <h2>{monthName}</h2>
+  <h2 class="title">
+    <div
+      class="arrow mr-2"
+      on:click={decrementMonth}
+      aria-label="Previous Month">
+      <svg
+        class="arrow"
+        focusable="false"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 320 512"><path
+          fill="currentColor"
+          d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z" /></svg>
+    </div>
+    {monthName}
+    <div class="arrow ml-2" on:click={incrementMonth} aria-label="Next Month">
+      > <svg
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 320 512"><path
+          fill="currentColor"
+          d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" /></svg>
+    </div>
+  </h2>
   <table class="table">
     <thead>
       <tr>
@@ -133,12 +195,12 @@
     <tbody>
       {#each days as daysInWeek}
         <tr>
-          {#each daysInWeek as { dayOfMonth, formattedDate, numDots }}
+          {#each daysInWeek as { dayOfMonth, date, formattedDate, numDots }}
             {#if !dayOfMonth}
               <td />
             {:else}
               <td
-                class:today={dayOfMonth === today}
+                class:today={date === today}
                 class:active={activeFile === formattedDate}
                 on:click={onClick}>
                 {dayOfMonth}
