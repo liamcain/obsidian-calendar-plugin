d11984115f95001b66a285bad86ed4da870b4003
Liam Cain <hi@liamca.in>
2021-03-21 15:00:04 -0400

    minor mobile fixes

--- src/ui/fileMenu.ts
+++ src/ui/fileMenu.ts
@@ -1,7 +1,7 @@
 import { App, Menu, Point, TFile } from "obsidian";
 
 export function showFileMenu(app: App, file: TFile, position: Point): void {
-  const fileMenu = new Menu();
+  const fileMenu = new Menu(app);
   fileMenu.addItem((item) =>
     item
       .setTitle("Delete")
